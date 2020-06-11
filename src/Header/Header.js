ab125ec9d5cb58d95a59bf90d49ca06b4337707a
m.kuchma <athenanebula@gmail.com>
2020-06-11 13:26:27 +0300

    align header images

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -15,6 +15,10 @@ function Header(){
             borderRadius: 8,
             marginTop: -5,
         },
+        logotrace: {
+            display: 'flex',
+            justifyContent: 'space-between'
+        },
         logo: {
             height: 40,
             width: 40
@@ -24,20 +28,23 @@ function Header(){
             width: 140
         },
         h2: {
-            margin: 0
+            margin: 0,
+            alignItems: 'center'
+        },
+
+        components: {
+            marginTop: 5,
+            marginLeft: 5,
+            marginRight: 5
         }
     }
 
     return(
         <div style={Styles.div}  className="Header">
-            <div>
-                <img src={logo} alt='logo' style={Styles.logo}/>
-            </div>
-            <div>
-                <h2 style={Styles.h2}>Hobbit Track</h2>
-            </div>
-            <div>
-                <img src={trace} alt='trace' style={Styles.trace}/>
+            <div style={Styles.logotrace}>
+                <div style={Styles.components}><img src={logo} alt='logo' style={Styles.logo}/></div>
+                <div style={Styles.components}><h2 style={Styles.h2}>Hobbit Track</h2></div>
+                <div style={Styles.components}><img src={trace} alt='trace' style={Styles.trace}/></div>
             </div>
             <div ><HeaderAuth /></div>
         </div>
