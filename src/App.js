c9015a1d47ff4742bbe0b65a0946c44eba5e90cb
m.kuchma <athenanebula@gmail.com>
2020-06-14 15:01:43 +0300

    return callback for authorization

--- src/App.js
+++ src/App.js
@@ -6,33 +6,13 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-        userID: '',
         listHabits: []
     }
     this.updateState = this.updateState.bind(this);
-    this.getListHabits = this.getListHabits.bind(this);
   }
 
   updateState(value){
-      this.setState({userID: value});
-      this.getListHabits(value);
-  }
-
-  getListHabits(id) {
-    let userID = this.state.userID
-        const URLF = `https://hobbittrackback.herokuapp.com/get_person_data?_id=${userID}`;
-
-        fetch(URLF).then(res =>{return res.json()})
-        .then(res=>{
-          //this.props.updateState(res)
-          //console.log(res);
-          this.setState({listHabits: res});
-          //console.log(this.state.listHabits)
-        })
-        .catch(function (error) {
-            alert("Login or password is wrong! 2")
-            console.log('Request failed 2', error)
-        });
+      this.setState({listHabits: value});
   }
 
   render () {
