2e90de21582471e3d615f92685a091beabb89a50
m.kuchma <athenanebula@gmail.com>
2020-06-18 19:51:01 +0300

    start request 'add new habit'

--- src/Body/AddHabit.js
+++ src/Body/AddHabit.js
@@ -18,8 +18,14 @@ class AddHabit extends Component {
 
     addNewHabit(event) {
         if (event.key === 'Enter' && this.state.newHabit !== '') {
+            let text = this.state.newHabit;
+            let _id = this.props.user_id;
+
+            console.log(text);
+            document.getElementById("addNewHabit").value = '';
             //todo: fetch на добавление
-            console.log(this.state.newHabit);
+            //request.args.get('_id'),request.args.get('name'),request.args.get('start'), request.args.get('end'))
+            //const URLF = `https://hobbittrackback.herokuapp.com/add_person_habit?_id=${}&name=${}&start=${}&end=${}`;
         }
     }
 
