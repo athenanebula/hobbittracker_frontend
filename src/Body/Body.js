05ba8240e3d697c0651903298437954ae043dce9
Shurik-Pavlyuchenkov <pavlyuchenkov.sasha@mail.ru>
2020-06-18 21:24:21 +0300

    change style for 'body'

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import HabitsList from './HabitsList'
+import "./Body.css"
 
 class Body extends Component{
     
@@ -8,35 +9,14 @@ render(){
         div: {
             display: 'flex',
             justifyContent: 'flex-end'
-        },
-
-        p: {
-            float: 'right',
-            marginTop: '20vw',
-            marginRight: '10vw',
-            maxWidth: '40vw',
-            fontSize: '1.7vw',
-            textAlign: 'center',
-            textShadow: ' 0 0 0.5px #a28d60',
-            color: '#43311e'
-        },
-
-        box: {
-            backgroundColor: 'rgba(37, 62, 3, 0.6)',
-            width: '60vw',
-            height: '40vw',
-            borderRadius: 60,
-            marginTop: '5vw',
-            marginRight: '5vw',
-            overflow: 'auto'
         }
     }
     return(
         <div id="startBody" className="Body" style={Styles.div}>
-            <div id="listHabitsBox" hidden style={Styles.box}>
+            <div id="listHabitsBox" className="box" hidden>
                 <HabitsList user_id = {this.props.user_id} listHabits = {this.props.listHabits} />
             </div>
-            <p id="startText" style={Styles.p}>Приветствуем тебя, уважаемый странник!<br/>
+            <p id="startText" className="startText" style={Styles.p}>Приветствуем тебя, уважаемый странник!<br/>
             На пути этого приложения тебя, как истинного хоббита, ждут испытания по выработке привычек.</p>
         </div>
     )}
