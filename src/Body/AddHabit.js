cf690aa4aa52724d9aa02fcf0411cfddfcc207bf
m.kuchma <athenanebula@gmail.com>
2020-06-18 16:11:30 +0300

    add 'add new habit' input handler for 'enter' button

--- src/Body/AddHabit.js
+++ src/Body/AddHabit.js
@@ -3,10 +3,30 @@ import React, {Component} from 'react'
 
 class AddHabit extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            newHabit: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.addNewHabit = this.addNewHabit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({newHabit: event.target.value});
+    }
+
+    addNewHabit(event) {
+        if (event.key === 'Enter' && this.state.newHabit !== '') {
+            //todo: fetch на добавление
+            console.log(this.state.newHabit);
+        }
+    }
+
     render(){        
         return(
             <div id="addHabit">
-                <input id="addNewHabit" name="newHabit" placeholder="Add New Habit"></input>
+                <input id="addNewHabit" name="newHabit" placeholder="Add New Habit" onChange={this.handleChange} onKeyPress={this.addNewHabit}></input>
             </div>
         )
     }
